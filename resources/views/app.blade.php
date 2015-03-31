9653c4f05deabfb049f2186a547ef205985525bf
unknown <eddie@Developer-6.apex.local>
2015-03-31 17:41:50 -0500

    first name lastname

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -42,7 +42,7 @@
 						<li><a href="/auth/login">Login</a></li>
 					@else
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->FirstName }} {{ Auth::user()->LastName }}<span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
 								<li><a href="/auth/logout">Logout</a></li>
 							</ul>
