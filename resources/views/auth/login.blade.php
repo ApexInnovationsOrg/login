d6500a40ffe1b734136350194e3b3604cf1a8cf4
unknown <eddie@Developer-6.apex.local>
2015-04-01 09:03:24 -0500

    no message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,7 +34,7 @@
 							</div>
 						</div>
 
-						<div class="form-group">
+						<div class="form-group" style="display:none">
 							<div class="col-md-6 col-md-offset-4">
 								<div class="checkbox">
 									<label>
