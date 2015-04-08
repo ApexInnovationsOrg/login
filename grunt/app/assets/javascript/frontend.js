2a50caee12ea0208671a2292dfb9e8276879a53d
unknown <eddie@Developer-6.apex.local>
2015-04-08 14:39:34 -0500

    added analytics

--- grunt/app/assets/javascript/frontend.js
+++ grunt/app/assets/javascript/frontend.js
@@ -62,10 +62,10 @@
 		});
 
 //google analytics
-  // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  // ga('create', 'UA-60099844-1', 'auto');
-  // ga('send', 'pageview');
+  ga('create', 'UA-42676756-1', 'apexinnovations.com');
+  ga('send', 'pageview');		
