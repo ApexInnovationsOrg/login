38d388080ff746495b23d37a3e45dcd0f4f9d4ab
unknown <John@Developer-1.apex.local>
2015-05-11 11:55:14 -0500

    Trying to trim Emails.

--- app/Auth/Passwords/PasswordBroker.php
+++ app/Auth/Passwords/PasswordBroker.php
@@ -182,7 +182,6 @@ class PasswordBroker extends BasePasswordBroker implements PasswordBrokerContrac
 	{
 		$credentials = array_except($credentials, ['token']);
 		//['Login'=>'eddie@apex']
-		dd($user);
 		$user = User::where('Login', '=' , $credentials['Login'])->first();
 		//20150330EM had to do a bit of hotwiring to make the CanResetPasswordContract happy. At the time of writing this, there's not a lot of documentation on altering a contract for Laravel 5, and this PHP is simply beyond my level of comprehension. 
 		if($user)
