c9e95d55450aab15fa3f71a62693163947a3bab1
eddimull <eddie@apexinnovations.com>
2021-01-08 12:26:25 -0600

    proxy support for https loop

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends HttpKernel {
         'Illuminate\View\Middleware\ShareErrorsFromSession',
         'App\Http\Middleware\SessionManager',
         'App\Http\Middleware\VerifyCsrfToken',
+        'Fideloper\Proxy\TrustProxies',
     ];
 
     /**
