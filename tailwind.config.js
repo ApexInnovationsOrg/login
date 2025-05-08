c23980d682d6550b4228cecf06a064094aca8e7c
JohnKlein985 <John@apexinnovations.com>
2025-05-08 08:57:20 -0500

    Adding redesign changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,14 @@ module.exports = {
                 sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
             },
         },
+        screens: {
+            'xs': '480px',    // Extra small devices
+            'sm': '640px',    // Small devices
+            'md': '768px',    // Medium devices
+            'lg': '1024px',   // Large devices
+            'xl': '1280px',   // Extra large devices
+            '2xl': '1536px',  // 2X large devices
+        },
     },
 
     variants: {
