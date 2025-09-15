0a47ed89ebfd0a93ea43dd6d6bdd3d641d03666b
Grigory Morozov <negrusti@gmail.com>
2025-09-15 11:20:59 +0700

    Classic form

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -5,30 +5,36 @@
         {{ status }}
     </div>
 
-    <form @submit.prevent="submit">
+    <!-- Plain form submit -->
+    <form method="POST" action="/login">
+        <input type="hidden" name="_token" :value="csrfToken" />
+
         <div>
             <breeze-label for="email" value="Email" />
-            <breeze-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username" />
+            <breeze-input id="email" type="email" class="mt-1 block w-full"
+                          name="email" required autofocus autocomplete="username" />
         </div>
 
         <div class="mt-4">
             <breeze-label for="password" value="Password" />
-            <breeze-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
+            <breeze-input id="password" type="password" class="mt-1 block w-full"
+                          name="password" required autocomplete="current-password" />
         </div>
 
         <div class="block mt-4">
             <label class="flex items-center">
-                <breeze-checkbox name="remember" v-model:checked="form.remember" />
+                <breeze-checkbox name="remember" />
                 <span class="ml-2 text-sm text-gray-600">Remember me</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <inertia-link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
+            <inertia-link v-if="canResetPassword" :href="route('password.request')" 
+                class="underline text-sm text-gray-600 hover:text-gray-900">
                 Forgot your password?
             </inertia-link>
 
-            <breeze-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+            <breeze-button class="ml-4">
                 Log in
             </breeze-button>
         </div>
@@ -36,45 +42,28 @@
 </template>
 
 <script>
-    import BreezeButton from '@/Components/Button'
-    import BreezeGuestLayout from "@/Layouts/Guest"
-    import BreezeInput from '@/Components/Input'
-    import BreezeCheckbox from '@/Components/Checkbox'
-    import BreezeLabel from '@/Components/Label'
-    import BreezeValidationErrors from '@/Components/ValidationErrors'
+import BreezeButton from '@/Components/Button'
+import BreezeGuestLayout from "@/Layouts/Guest"
+import BreezeInput from '@/Components/Input'
+import BreezeCheckbox from '@/Components/Checkbox'
+import BreezeLabel from '@/Components/Label'
+import BreezeValidationErrors from '@/Components/ValidationErrors'
 
-    export default {
-        layout: BreezeGuestLayout,
+export default {
+    layout: BreezeGuestLayout,
 
-        components: {
-            BreezeButton,
-            BreezeInput,
-            BreezeCheckbox,
-            BreezeLabel,
-            BreezeValidationErrors
-        },
+    components: {
+        BreezeButton,
+        BreezeInput,
+        BreezeCheckbox,
+        BreezeLabel,
+        BreezeValidationErrors
+    },
 
-        props: {
-            canResetPassword: Boolean,
-            status: String,
-        },
-
-        data() {
-            return {
-                form: this.$inertia.form({
-                    email: '',
-                    password: '',
-                    remember: false
-                })
-            }
-        },
-
-        methods: {
-            submit() {
-                this.form.post(this.route('login'), {
-                    onFinish: () => this.form.reset('password'),
-                })
-            }
-        }
+    props: {
+        canResetPassword: Boolean,
+        status: String,
+        csrfToken: String,   // provided by middleware
     }
+}
 </script>
