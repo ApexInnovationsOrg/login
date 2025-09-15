676763c744b1b067687ddaa5ea37c985330ce187
Grigory Morozov <negrusti@gmail.com>
2025-09-15 09:16:31 +0700

    Override Fortify's login redirect response

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Fortify\Contracts\LoginResponse;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // Override Fortify's login redirect
+        app()->singleton(LoginResponse::class, function () {
+            return new class implements LoginResponse {
+                public function toResponse($request)
+                {
+                    return redirect()->away('https://www.apexinnovations.com/MyCurriculum.php');
+                }
+            };
+        });
     }
 }
