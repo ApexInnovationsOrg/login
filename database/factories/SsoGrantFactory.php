04302e5fbd410a345af372948cc6bebbed910a47
eddimull <eddimull@gmail.com>
2026-07-07 05:37:21 -0500

    Add sso_grants table and model for org-scoped SSO delegation

---
+++ database/factories/SsoGrantFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\SsoGrant;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SsoGrantFactory extends Factory
+{
+    protected $model = SsoGrant::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'organization_id' => 1,
+            'granted_by' => '1:Test Admin',
+        ];
+    }
+}
